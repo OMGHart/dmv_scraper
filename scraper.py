6724e61cc50deed8ff27e29d0267f55e4061061b
hart <mjh912@gmail.com>
2025-07-15 07:43:08 -0400

    Testing time zones.

--- scraper.py
+++ scraper.py
@@ -79,8 +79,8 @@ def scrape_all_locations():
     tz_utc = timezone.utc
     tz_eastern = timezone(timedelta(hours=-4))
     # now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    now = datetime.now().astimezone(tzinfo=tz_utc)
-    now = now.astimezone(tz_eastern)
+    now = datetime.now().astimezone(tz_eastern)
+    # now = now.astimezone(tz_eastern)
     for name, url in LOCATIONS.items():
         try:
             wait_time = scrape_wait_time(url)
